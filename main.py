b1be2ec927bcfdb5ae398910741916a7776acbbd
rvillarrealcp <richard.villarreal2@hotmail.com>
2025-10-29 11:19:02 -0500

    Assignment complete with counts

--- main.py
+++ main.py
@@ -1,25 +1,33 @@
 # 🧠 Your Task: Tokenize the story from 'story.txt' using NLTK's sent_tokenize and word_tokenize functions
 
 # Make sure required NLTK resources are available
-
+import re
+import nltk
+nltk.download('punkt_tab')
+from nltk.tokenize import sent_tokenize
+from nltk.tokenize import word_tokenize
 
 # 1. Open and read the story text
+text_file = open("./story.txt")
+file_val = text_file.read()
 
 
 # 2. (Optional) Remove any unwanted characters using re.sub
 # For example: remove extra whitespace or punctuation symbols
-clean_story = None  # Keep common sentence characters
+clean_story = re.sub(r'\s+', " ", file_val).strip()  # Keep common sentence characters
 
 # 3. Tokenize the story into sentences
 # TODO: Replace the line below with a call to sent_tokenize
-sentences = []
+sentences = sent_tokenize(clean_story)
+# print(sentences)
 
 # 4. Tokenize the story into words
 # TODO: Replace the line below with a call to word_tokenize
-words = []
-
+words = word_tokenize(clean_story)
 # 5. Print results
 print("=== Sentences ===")
 print(sentences)
 print("\n=== Words ===")
 print(words)
+print(f'Number of Words: {len(words)}')
+print(f'Number of Sentences: {len(sentences)}')
