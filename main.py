c76aaccdeb4fd10453f0b55a35ea4d917e0ead96
rvillarrealcp <richard.villarreal2@hotmail.com>
2025-10-29 16:06:19 -0500

    Cleaner Version

--- main.py
+++ main.py
@@ -3,27 +3,30 @@
 # Make sure required NLTK resources are available
 import re
 import nltk
+from nltk.tokenize import sent_tokenize, word_tokenize
 nltk.download('punkt_tab')
-from nltk.tokenize import sent_tokenize
-from nltk.tokenize import word_tokenize
+
 
 # 1. Open and read the story text
 text_file = open("./story.txt")
-file_val = text_file.read()
+file_val = (text_file.read()).lower()
+text_file.close()
 
 
 # 2. (Optional) Remove any unwanted characters using re.sub
 # For example: remove extra whitespace or punctuation symbols
-clean_story = re.sub(r'\s+', " ", file_val).strip()  # Keep common sentence characters
+clean_story = re.sub(r'\s+', ' ', file_val).strip()  # Keep common sentence characters
+cleaner_story = re.sub(r' *\.', ".", clean_story)
+cleanest_story = re.sub(r'[^\w\.\s]', '', cleaner_story)
 
 # 3. Tokenize the story into sentences
 # TODO: Replace the line below with a call to sent_tokenize
-sentences = sent_tokenize(clean_story)
+sentences = sent_tokenize(cleanest_story)
 # print(sentences)
 
 # 4. Tokenize the story into words
 # TODO: Replace the line below with a call to word_tokenize
-words = word_tokenize(clean_story)
+words = word_tokenize(cleanest_story)
 # 5. Print results
 print("=== Sentences ===")
 print(sentences)
